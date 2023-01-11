a8a4f29f8cc41004fa334bb2b498628cbd67929d
Taufiq Abdullah <taufiqtab77@gmail.com>
2023-01-11 09:11:43 +0700

    dulrestful init

---
+++ controllers/productcontroller/productcontroller.go
@@ -0,0 +1,46 @@
+package productcontroller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/taufiqtab/dulrestful/models"
+	"gorm.io/gorm"
+)
+
+func Index(c *gin.Context) {
+	var products []models.Product
+
+	models.DB.Find(&products)
+	c.JSON(http.StatusOK, gin.H{"products": products})
+}
+
+func Show(c *gin.Context) {
+	var product models.Product
+	id := c.Param("id")
+
+	if err := models.DB.First(&product, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "data tidak ditemukan"})
+			return
+		default:
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": err.Error()})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"product": product})
+}
+
+func Create(c *gin.Context) {
+
+}
+
+func Update(c *gin.Context) {
+
+}
+
+func Delete(c *gin.Context) {
+
+}
